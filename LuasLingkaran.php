bd7e0db79bfa9e39381148816da26adac25a386b
Narislah Mauludina <nrslhmldna@gmail.com>
2025-11-08 15:30:04 +0700

    pake method

--- LuasLingkaran.php
+++ LuasLingkaran.php
@@ -3,13 +3,21 @@
 class LuasLingkaran {
     public const phi = 3.14;
     public int $jari;
+
+    public function tampil($nama = 'ban') {    //pake method tampil
+        $rumus = LuasLingkaran::phi * ($this -> jari * $this -> jari);
+        echo "lingkaran {$nama} hasilnya adalah : $rumus";
+    }
+
+    public static function testing () {
+        echo "<br/>";
+        echo "ini dari static";     //method static
+    }
 }
 
 $Lingkaran = new LuasLingkaran();
-$Lingkaran -> jari = 16;
-
-$rumus = LuasLingkaran::phi * $Lingkaran->jari * $Lingkaran->jari;
-
-echo "hasilnya adalah: ". $rumus;
+$Lingkaran -> jari = 7;
+$Lingkaran -> tampil('roda');  //pemanggilan method tampil
+LuasLingkaran::testing();  //pemanggilan method static
 
 ?>
